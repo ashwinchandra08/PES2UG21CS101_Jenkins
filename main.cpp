a162e178d0ab2c01e70eb7ec6ffa73af09571c3c
ashwinchandra08 <110295167+ashwinchandra08@users.noreply.github.com>
2024-03-08 23:47:34 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Display a message
+    cout << "Hello, this is PES2UG21CS101-1.cpp!" << endl;
+
+    // Perform some basic operations
+    int a = 5;
+    int b = 7;
+    int sum = a + b;
+
+    // Display the result
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+
+    return 0;
+}
